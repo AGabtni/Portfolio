81ee0745da75be599f1c1a380cc6b600627f6872
Ahmed <Ahmed@VNR1020-001.uottawa.o.univ>
2020-02-03 12:01:07 -0500

    postprocessing and threejs are correcty set up

--- src/App.js
+++ src/App.js
@@ -1,42 +1,123 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
-import { EffectComposer, RenderPass, GlitchPass } from "postprocessing";import * as THREE from "three";
+import * as THREE from "three";
 
-
+import {ShaderPass} from "three/examples/jsm/postprocessing/ShaderPass"
+import {EffectComposer} from "three/examples/jsm/postprocessing/EffectComposer"
+import {RenderPass} from "three/examples/jsm/postprocessing/RenderPass"
+import {DotScreenShader } from "three/examples/jsm/shaders/DotScreenShader"
 
 var scene = new THREE.Scene();
-var renderer;
-var composer ;
+let renderer;
+let  composer ;
+let camera;
+let object
+let light
+init();
 function init(){
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+
+  //--Renderer init : 
+
+  // renderer = new THREE.WebGLRenderer();
+  // renderer.setPixelRatio( window.devicePixelRatio );
+  // renderer.setSize( window.innerWidth, window.innerHeight );
+  // document.body.appendChild( renderer.domElement );
+  
+  const canvas = document.querySelector('#c');
+	canvas.width = document.body.clientWidth;
+  canvas.height = document.body.clientHeight;
+  renderer = new THREE.WebGLRenderer({canvas});
+
+  
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.z = 400;
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.Fog( 0x000000, 1, 1000 );
+  scene.add(camera)
+
+  object = new THREE.Object3D();
+  scene.add( object );
+
+
+  var geometry = new THREE.SphereBufferGeometry( 1, 4, 4 );
+  var material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
+
+  for ( var i = 0; i < 100; i ++ ) {
+
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 ).normalize();
+    mesh.position.multiplyScalar( Math.random() * 400 );
+    mesh.rotation.set( Math.random() * 2, Math.random() * 2, Math.random() * 2 );
+    mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50;
+    object.add( mesh );
+
+  }
+
+  scene.add( new THREE.AmbientLight( 0x222222 ) );
+  light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 1, 1, 1 );
+  scene.add( light );
+  console.log("grgnr")
+
+
+
+  //--Postprocessing : 
   composer = new EffectComposer(renderer);
+
+
+  //Dot postprocessing just for testing :
+  composer.addPass(new RenderPass(scene,camera))
+
+  var effect = new ShaderPass( DotScreenShader  );
+				effect.uniforms[ 'scale' ].value = 4;
+				composer.addPass( effect );
+
+
+  window.addEventListener( 'resize', onWindowResize, false );
+  animate()
+
+}
+
+function animate() {
+
+  render();
+  requestAnimationFrame( animate );
+
+  object.rotation.x += 0.005;
+  object.rotation.y += 0.01;
+  composer.render();
 
 
 }
 
-init();
+function render() {
+
+
+
+  renderer.render(scene, camera);
+
+
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  composer.setSize( window.innerWidth, window.innerHeight );
+}
+
 
 function App() {
   return (
     <div className="App">
       
       <header className="App-header">
-        <NavBar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
